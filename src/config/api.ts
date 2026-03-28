81c05cbb51abae3ac3a7b82d09b5e6b0c39a9e8d
driikoK <kupchak.a.m@gmail.com>
2026-03-28 21:01:32 +0200

    Add environment

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,10 @@
 import axios from 'axios';
 import { useAdminAuthStore } from '../store/adminAuthStore';
 
+const API_BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
